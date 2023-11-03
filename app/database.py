e4cccb234f888edf82eba08e79059d98f7e4804a
itz-amethyst <pransermi@gmail.com>
2023-11-03 21:36:02 +0330

    finally !!

--- app/database.py
+++ app/database.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote_plus
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -6,13 +8,16 @@ from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
-postgres_username = getenv('POSTGRES_USERNAME', default = 'Test')
-postgres_pass = getenv('POSTGRES_PASSWORD', default = 'Test')
-postgres_hostname = getenv('POSTGRES_HOSTNAME', default = 'Test')
-postgres_databasename = getenv('POSTGRES_DBNAME', default = 'Test')
+postgres_username = getenv('POSTGRES_USERNAME')
+postgres_pass = getenv('POSTGRES_PASSWORD')
+postgres_hostname = getenv('POSTGRES_HOSTNAME')
+postgres_databasename = getenv('POSTGRES_DBNAME')
+
+#! Note: -> my postgres pass has unique characters like @ ! # .... should use quote_plus  !
+
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip-address/OR/hostname>/<database_name>"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{postgres_username}:{postgres_pass}@{postgres_hostname}/{postgres_databasename}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{postgres_username}:%s@{postgres_hostname}/{postgres_databasename}" % quote_plus(postgres_pass)
 
 
 #  connect_args={"check_same_thread": False} -> For sqlite db
