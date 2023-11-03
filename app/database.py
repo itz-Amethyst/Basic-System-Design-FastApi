a1228cc85070300a5a5dc54296e5a4375a6018cb
itz-amethyst <pransermi@gmail.com>
2023-11-03 20:46:19 +0330

    feat: configured postgres db

--- app/database.py
+++ app/database.py
@@ -2,8 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip-address/hostname>/<database_name>"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+postgres_username = getenv('POSTGRES_USERNAME', default = 'Test')
+postgres_pass = getenv('POSTGRES_PASSWORD', default = 'Test')
+postgres_hostname = getenv('POSTGRES_HOSTNAME', default = 'Test')
+postgres_databasename = getenv('POSTGRES_DBNAME', default = 'Test')
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip-address/OR/hostname>/<database_name>"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{postgres_username}:{postgres_pass}@{postgres_hostname}/{postgres_databasename}"
 
 
 #  connect_args={"check_same_thread": False} -> For sqlite db
@@ -13,3 +22,11 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
