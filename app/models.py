2a83f70937630ab34dda7bb6176f10c8021df68b
itz-amethyst <pransermi@gmail.com>
2023-11-03 20:45:37 +0330

    moved

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column , Integer , String , Boolean
+from app.database import Base
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key = True, nullable = False)
+    title = Column(String, nullable = False)
+    content = Column(String(250), nullable = False)
+    published = Column(Boolean, nullable = False, server_default = False)
+    rating = Column(Integer(5), nullable = False)
