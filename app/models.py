e6df07fa62c687e485587cf379b56f7e11e57e6e
itz-amethyst <pransermi@gmail.com>
2023-11-06 21:09:21 +0330

    feat: Added ma custom alg for user id table

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
+
 from datetime import datetime
 
 from sqlalchemy import Column , Integer , String , Boolean , TIMESTAMP
-from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.expression import text, func
 from app.database import Base
+from utils.CustomMethods import generate_generic_id
+
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -13,3 +16,18 @@ class Post(Base):
     published = Column(Boolean, nullable = False, server_default = "False")
     rating = Column(Integer, nullable = False,)
     created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = text('now()'), default = datetime.now())
+
+
+
+
+
+class User(Base):
+    __tablename__ = 'Users'
+
+    #! Imp Note: It's not set on server default in db so whenever you create a custom data in db  dont except to auto generate id
+    id = Column(String, primary_key = True, nullable = False, unique = True, default = generate_generic_id)
+    email = Column(String, nullable = False, unique = True)
+    password = Column(String, nullable = False)
+    created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = text('now()'), default = datetime.now())
+
+
--- utils/CustomMethods.py
+++ utils/CustomMethods.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 def find_post(id , source):
     for p in source:
         if p['id'] == id:
@@ -7,3 +11,11 @@ def find_index_post(id, source):
     for i,p in enumerate(source):
         if p['id'] == id:
             return i
+
+
+def generate_generic_id():
+    letters = string.ascii_letters
+    numbers = string.digits
+    random_chars = random.choices(letters + numbers , k = 12)
+    random_string = ''.join(random_chars)
+    return f"XOM-{random_string}"
