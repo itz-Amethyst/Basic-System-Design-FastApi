1c838ca0046d8f5c77e2c73bc368f28e983679d5
itz-amethyst <pransermi@gmail.com>
2023-11-08 18:43:56 +0330

    feat: Hashing operations

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 
 from . import models
 from .database import engine
-from .routers import post, user
+from .routers import post, user , auth
 
 models.Base.metadata.create_all(bind = engine)
 
@@ -17,6 +17,7 @@ models.Base.metadata.create_all(bind = engine)
 app = FastAPI(swagger_ui_parameters = {"syntaxHighlight.theme": "nord"})
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get('/')
 async def root():
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 
+from utils.Hashes import verify_password
 from .. import database, schemas, models
 
 router = APIRouter(
@@ -10,7 +11,10 @@ router = APIRouter(
 @router.post("/login")
 def login(user_credentials: schemas.UserLogin ,db: Session = Depends(database.get_db)):
 
-    user = db.query(models.User).get(models.User.email == user_credentials.email)
+    user: models.User = db.query(models.User).get(models.User.email == user_credentials.email)
 
     if not user:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Invalid Credentials")
+
+    if not verify_password(user_credentials.password, user.password):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Invalid Credentials")
