ddf65216d5d2260d1408fbb99efaa91033e6971f
itz-amethyst <pransermi@gmail.com>
2023-11-10 20:36:04 +0330

    refactor

--- app/database.py
+++ app/database.py
@@ -3,21 +3,14 @@ from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from shared import settings
 
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()
-postgres_username = getenv('POSTGRES_USERNAME')
-postgres_pass = getenv('POSTGRES_PASSWORD')
-postgres_hostname = getenv('POSTGRES_HOSTNAME')
-postgres_databasename = getenv('POSTGRES_DBNAME')
 
 #! Note: -> my postgres pass has unique characters like @ ! # .... should use quote_plus  !
 
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip-address/OR/hostname>/<database_name>"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{postgres_username}:%s@{postgres_hostname}/{postgres_databasename}" % quote_plus(postgres_pass)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_HOSTNAME}:%s@{settings.POSTGRES_HOSTNAME}/{settings.POSTGRES_DBNAME}" % quote_plus(settings.POSTGRES_PASSWORD)
 
 
 #  connect_args={"check_same_thread": False} -> For sqlite db
--- app/oauth2.py
+++ app/oauth2.py
@@ -4,29 +4,26 @@ from datetime import datetime , timedelta
 from . import schemas
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from shared import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = 'login')
 
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES )
     to_encode.update({"exp": expire})
 
     # ! By reading from algorithm class or directly by variable
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHMS.HS256)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm = ALGORITHMS.HS256)
 
     return encoded_jwt
 
 def verify_access_token(token: str, credentials_exception):
 
     try:
-        payload = jwt.decode(token , SECRET_KEY , algorithms = [ALGORITHM])
+        payload = jwt.decode(token , settings.SECRET_KEY , algorithms = [settings.ALGORITHM])
 
         user_id = payload.get("user_id")
 
