a555b5036dd4ca0cce5f9c26f93f8fa35500bd95
itz-amethyst <pransermi@gmail.com>
2023-11-12 18:25:03 +0330

    feat: added allowed origins

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 # from typing import List
 
 from .routers import post, user , auth , vote
 from app.db.database import engine , metadata
+from .shared import settings
 
 # NOTE: This should be here when you start the app it will run main.py so to create models
-metadata.create_all(bind = engine)
+#! No longer need when you have alembic
+# metadata.create_all(bind = engine)
 
 
 # /doc => swagger
@@ -15,6 +17,18 @@ metadata.create_all(bind = engine)
 # Link: https://fastapi.tiangolo.com/how-to/configure-swagger-ui/
 # Theme: obsidian
 app = FastAPI(swagger_ui_parameters = {"syntaxHighlight.theme": "nord"})
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        # You can use this or otherwise use * to allow all
+        allow_origins = [str(origin) for origin in settings.BACKEND_CORS_ORIGINS] ,
+        allow_credentials = True ,
+        allow_methods = ["*"] ,
+        allow_headers = ["*"] ,
+    )
+
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
--- app/shared/__init__.py
+++ app/shared/__init__.py
@@ -1,4 +1,6 @@
+from typing import List , Union
 
+from pydantic import AnyHttpUrl , field_validator
 from pydantic_settings import BaseSettings , SettingsConfigDict
 from dotenv import load_dotenv
 
@@ -15,6 +17,16 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @field_validator("BACKEND_CORS_ORIGINS" , pre = True)
+    def assemble_cors_origins( cls , v: Union[str , List[str]] ) -> Union[List[str] , str]:
+        if isinstance(v , str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v , (list , str)):
+            return v
+        raise ValueError(v)
 
     # class Config:
     #     env_file = '.env'
