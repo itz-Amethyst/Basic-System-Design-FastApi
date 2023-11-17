9385dab64b45c99a50339b479ff8e3fa5152f737
itz-amethyst <pransermi@gmail.com>
2023-11-17 22:02:34 +0330

    IDK

--- utils/AvatarUpload.py
+++ utils/AvatarUpload.py
@@ -1,7 +1,9 @@
 from fastapi import UploadFile , File , HTTPException , status
 from secrets import token_hex
 from pathlib import Path
+import shutil
 
+# IDK What's wrong with this kinda MF
 
 MB = 1024 * 1024
 Upload_Dir = Path() / 'Uploads/Avatars'
@@ -16,13 +18,14 @@ def Upload_Avatar(file: UploadFile = File(...)):
     if not 0 < len(file.file.read()) <= 1 * MB:
         raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST, detail = "The Size must be between 0 - 1 Mb")
 
-    file_ext = file.filename.split(".").pop()  # to get only name file f.e.g image22.png => image22
+    file_ext = file.filename.split(".").pop()  # to get format like .jpg .png ...
     file_name = token_hex(10)
-    file_path = Upload_Dir / f"{file_name}.{file_ext}"
+    file_path = f"{file_name}.{file_ext}"
 
     with open(file_path, 'wb') as f:
-        content = file.file.read()
-        f.write(content)
+        shutil.copyfileobj(file.file, f)
+        # content = await file.file.read()
+        # f.write(content)
 
     return {"success": True, "file_path": file_path, 'message': "File Uploaded successfully", 'size': file.size}
 
