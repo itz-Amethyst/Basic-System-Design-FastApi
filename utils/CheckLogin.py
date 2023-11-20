a3bc5b5d8e02b33b02bb3ad85d957916f845320d
itz-amethyst <pransermi@gmail.com>
2023-11-20 21:42:50 +0330

    feat: set user_state

--- utils/CheckLogin.py
+++ utils/CheckLogin.py
@@ -3,12 +3,13 @@ from app.db.database import get_db
 from app.db.models import User
 from utils.Hashes import verify_password
 
+db = next(get_db())
+
 
 # Created for adminV2
 
 def login_util(username:str , password: str) -> bool:
 
-    db = next(get_db())
 
     user = db.query(User).filter(User.email == username).first()
 
@@ -20,3 +21,13 @@ def login_util(username:str , password: str) -> bool:
 
 
     return True
+
+def set_user_state(username):
+
+    user = db.query(User).filter(User.email == username).first()
+
+    #? Fix This later by adding default image in database so no need this after that
+    if user.image_path is None:
+        user.image_path = 'https://cdn.discordapp.com/attachments/921633563810627588/1175350979202400267/image.png?ex=656ae9e6&is=655874e6&hm=002a637e7bcbf71940f99b778a16e15f6d7487ddd05d11be3020bf04eebae2e0&'
+
+    return {'username': user.email, 'avatar': user.image_path}
