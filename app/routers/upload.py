ecb6231d2f16e5deacf7adb473a598438fb0b5e7
itz-amethyst <pransermi@gmail.com>
2023-12-02 18:44:03 +0330

    feat: api to get all urls in sw3

--- app/routers/upload.py
+++ app/routers/upload.py
@@ -16,3 +16,8 @@ def upload_file(request: Request, file: UploadFile = File(..., description='Prov
 
 
     return result
+
+@router.get('/', status_code = status.HTTP_200_OK)
+def get_browsable_urls_in_sw3():
+
+    return UploadManager.get_browsable_urls_in_sw3()
