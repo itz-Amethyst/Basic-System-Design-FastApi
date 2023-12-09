9ca82911e85532884528ad170487d2775f7fb30c
itz-amethyst <pransermi@gmail.com>
2023-12-09 18:07:43 +0330

    fix: Fixed create user through terminal

--- app/commands/create_super_user.py
+++ app/commands/create_super_user.py
@@ -1,23 +1,44 @@
 import asyncclick as click
-from pydantic import EmailStr
-from fastapi import Request
+from sqlalchemy.orm import Session
 
-from app.db.models.user import RoleOptions
-from app.managers.user import UserManager
+from app.db.database import get_db
+from app.db.models.user import RoleOptions , User
+from email_validator import validate_email
+
+from utils.Hashes import hash_password
+
+db: Session = next(get_db())
 
 
 #! Need to work on it
 @click.command()
 @click.option("-e", "--email", type=str, required = True)
 @click.option("-p", "--password", type=str, required = True)
-@click.option("-r", "--role", type=RoleOptions, required = True)
-async def create_user(email: EmailStr, password: str, role: RoleOptions):
+@click.option("-r", "--role", type=str, required = False)
+def create_user(email: str, password: str, role: str):
 
-    user_data = {"email": email, "password": password, "role": RoleOptions.value, "profile_picture": "https://cdn.discordapp.com/attachments/921633563810627588/1175350979202400267/image.png?ex=656ae9e6&is=655874e6&hm=002a637e7bcbf71940f99b778a16e15f6d7487ddd05d11be3020bf04eebae2e0&"}
+    if role is None:
+        role = "super_admin"
 
-    await UserManager.register(user_data = user_data , request = Request)
+    if validate_email(email) is False:
+        raise RuntimeError("email is not valid")
 
+    print(role)
+    if role.lower() not in RoleOptions.__members__:
+        raise RuntimeError("Role Not Valid please select between : user, super_admin, admin")
 
+    check_user = db.query(User).filter(User.email == email).first()
+
+    if check_user:
+        raise RuntimeError(f"User with this email {email} already exists")
+
+    password = hash_password(password)
+
+    new_user: User = User(email = email , password = password , image_path = 'G:/Fast Api/Basic System/Uploads/d4c92f13ff1627393532043.jpg' ,
+                          size = 0 , ext = "unknown" , mime = "unknown", role = role)
+    db.add(new_user)
+    db.commit()
+    click.echo("User Successfully created!")
 
 
 if __name__ == "__main__":
