8ac9d78ba3ba3a9d26dad78d3952e66287c3ca9f
itz-amethyst <pransermi@gmail.com>
2023-12-15 20:06:19 +0330

    refactor: redis package combined 2 method connection into 1

--- app/package/redis.py
+++ app/package/redis.py
@@ -1,36 +1,59 @@
 """redis.py"""
-import os
-from typing import Tuple
+from typing import Tuple , Callable , Optional
 
 import redis
 
 from .enums import RedisStatus
 
 
-def redis_connect(host_url: str, local: bool, password:str, port:int) -> Tuple[RedisStatus, redis.client.Redis]:
-    """Attempt to connect to `host_url` depends on `local` if it sets to `true` it will connect to `cloud` with `password` and return a Redis client instance if successful."""
-    return _connect(host_url) if local is True else _connect_cloud(host_url, password, port)
-        # if os.environ.get("CACHE_ENV") != "TEST" else _connect_fake()
 
+def redis_connect(host_url: str, local: bool, password: str, port: int) -> Tuple[RedisStatus, redis.client.Redis]:
+    """Attempt to connect to `host_url`. If `local` is set to `True`, it will connect to `cloud` with `password` and return a Redis client instance if successful."""
+    connection_method = redis.from_url if not local else redis.Redis
+    return _connect_generic(connection_method, host_url, password=password, port=port)
 
-def _connect(host_url: str) -> Tuple[RedisStatus, redis.client.Redis]:  # pragma: no cover
+def _connect_generic(connection_method: Callable, *args, **kwargs) -> Tuple[RedisStatus, Optional[redis.client.Redis]]:
     try:
-        redis_client = redis.from_url(host_url)
-        if redis_client.ping():
-            return RedisStatus.CONNECTED, redis_client
-        return RedisStatus.CONN_ERROR, None
-    except redis.AuthenticationError:
-        return RedisStatus.AUTH_ERROR, None
-    except redis.ConnectionError:
+        redis_client = connection_method(*args, **kwargs)
+    except (redis.AuthenticationError, redis.ConnectionError):
         return RedisStatus.CONN_ERROR, None
 
-def _connect_cloud(host_url: str, password:str, port:int):
-    try:
-        redis_client = redis.Redis(host = host_url, password = password, port = port)
-        if redis_client.ping():
-            return RedisStatus.CONNECTED, redis_client
-        return RedisStatus.CONN_ERROR, None
-    except redis.AuthenticationError:
-        return RedisStatus.AUTH_ERROR, None
-    except redis.ConnectionError:
-        return RedisStatus.CONN_ERROR, None
+    if redis_client.ping():
+        return RedisStatus.CONNECTED, redis_client
+    return RedisStatus.CONN_ERROR, None
+
+
+
+
+
+
+
+
+
+# def redis_connect(host_url: str, local: bool, password:str, port:int) -> Tuple[RedisStatus, redis.client.Redis]:
+#     """Attempt to connect to `host_url` depends on `local` if it sets to `true` it will connect to `cloud` with `password` and return a Redis client instance if successful."""
+#     return _connect(host_url) if local is True else _connect_cloud(host_url, password, port)
+#         # if os.environ.get("CACHE_ENV") != "TEST" else _connect_fake()
+#
+#
+# def _connect(host_url: str) -> Tuple[RedisStatus, redis.client.Redis]:  # pragma: no cover
+#     try:
+#         redis_client = redis.from_url(host_url)
+#         if redis_client.ping():
+#             return RedisStatus.CONNECTED, redis_client
+#         return RedisStatus.CONN_ERROR, None
+#     except redis.AuthenticationError:
+#         return RedisStatus.AUTH_ERROR, None
+#     except redis.ConnectionError:
+#         return RedisStatus.CONN_ERROR, None
+#
+# def _connect_cloud(host_url: str, password:str, port:int):
+#     try:
+#         redis_client = redis.Redis(host = host_url, password = password, port = port)
+#         if redis_client.ping():
+#             return RedisStatus.CONNECTED, redis_client
+#         return RedisStatus.CONN_ERROR, None
+#     except redis.AuthenticationError:
+#         return RedisStatus.AUTH_ERROR, None
+#     except redis.ConnectionError:
+#         return RedisStatus.CONN_ERROR, None
