ebdcbc23bc41ceacee46a6749d8096977c25256e
itz-amethyst <pransermi@gmail.com>
2023-12-17 20:27:55 +0330

    fix: cache package > fixed read data from redis format

--- app/package/cache.py
+++ app/package/cache.py
@@ -1,5 +1,6 @@
 """cache.py"""
 import asyncio
+import json
 from datetime import timedelta
 from functools import partial , wraps , update_wrapper
 from http import HTTPStatus
@@ -58,19 +59,30 @@ def cache(*, expire: Union[int, timedelta] = ONE_YEAR_IN_SECONDS):
                     response.status_code = int(HTTPStatus.NOT_MODIFIED)
                     return create_response(response, None, create_response_directly)
 
-                return create_response(response, in_cache, create_response_directly)
+                # Convert bytes key to json
+                # converted_json_data = json.loads(in_cache)
+                # converted_json_data = converted_json_data[key] if key in converted_json_data else converted_json_data
+
+                converted_json_data = json.loads(in_cache).get(key , json.loads(in_cache))
+
+                return create_response(response, str(converted_json_data), create_response_directly)
 
             response_data = await get_api_response_async(func, *args, **kwargs)
             ttl = calculate_ttl(expire)
-            cached = redis_cache.add_to_cache(key, response_data, ttl)
+            cached, serialized_dict = redis_cache.add_to_cache(key, response_data, ttl)
 
             if cached:
                 redis_cache.set_response_headers(
                     response, cache_hit=False, response_data=response_data, ttl=ttl
                 )
-                return create_response(
-                    response, serialize_json(response_data), create_response_directly
-                )
+                if hasattr(response_data, "__len__"):
+                    return create_response(
+                        response, serialize_json(response_data), create_response_directly
+                    )
+                else:
+                    return create_response(
+                        response , serialized_dict , create_response_directly
+                    )
 
             return response_data
 
@@ -99,10 +111,10 @@ def create_response(response, content, create_directly):
     """Creates a FastAPI response."""
     if create_directly:
         return Response(
-            content=content,
-            media_type="application/json",
-            headers=response.headers,
-            status_code=response.status_code,
+            content = content ,
+            media_type = "application/json" ,
+            headers = {"Content-Length": str(len(content))} ,
+            status_code = response.status_code ,
         )
     return content
 
